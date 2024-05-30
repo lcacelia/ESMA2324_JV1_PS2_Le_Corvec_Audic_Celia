f3f57a3707355629ca18c353fdd3a7412c7c7029
Marwan <lecorvecaudiccelia@gmail.com>
2024-05-30 11:09:58 +0200

    sauvegarde

--- Assets/Script/Autre/paralaxe.cs
+++ Assets/Script/Autre/paralaxe.cs
@@ -18,7 +18,10 @@ public class paralaxe : MonoBehaviour
     void FixedUpdate()
     {
         float dist = (cam.transform.position.x * parallaxEffect);
-
+        float temp = (cam.transform.position.x * (1 - parallaxEffect));
         transform.position = new Vector3(startpos + dist, transform.position.y, transform.position.z);
+
+        if (temp > startpos + length) startpos += length;
+        else if (temp < startpos - length) ;
     }
 }
